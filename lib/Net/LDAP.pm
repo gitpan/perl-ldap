ea6513799bcfaf471bc3185faee0a7072b91fccf
Graham Barr <gbarr@pobox.com>
2011-09-03 12:48:31 +0000

    Import of GBARR/perl-ldap-0.42 from CPAN.
    gitpan-cpan-distribution: perl-ldap
    gitpan-cpan-version:      0.42
    gitpan-cpan-path:         GBARR/perl-ldap-0.42.tar.gz
    gitpan-cpan-author:       GBARR
    gitpan-cpan-maturity:     released

--- lib/Net/LDAP.pm
+++ lib/Net/LDAP.pm
@@ -28,7 +28,7 @@ use Net::LDAP::Constant qw(LDAP_SUCCESS
 			   LDAP_UNAVAILABLE
 			);
 
-$VERSION 	= "0.41";
+$VERSION 	= "0.42";
 @ISA     	= qw(Tie::StdHash Net::LDAP::Extra);
 $LDAP_VERSION 	= 3;      # default LDAP protocol version
 
@@ -385,7 +385,10 @@ sub bind {
       # If we're talking to a round-robin, the canonical name of
       # the host we are talking to might not match the name we
       # requested
-      my $connected_name = $ldap->{net_ldap_socket}->peerhost;
+      my $connected_name;
+      if ($ldap->{net_ldap_socket}->can('peerhost')) {
+        $connected_name = $ldap->{net_ldap_socket}->peerhost;
+      }
       $connected_name ||= $ldap->{net_ldap_host};
 
       $sasl_conn = eval {
